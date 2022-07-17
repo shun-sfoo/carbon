34e4cf66c9db7c1fbd9cb697d53d70d12263fb07
shun-sfoo <ganymede0915@gmail.com>
2022-07-17 22:13:46 +0800

    a good name

---
+++ c_programming_language/ch1/a11e1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("hello, world\n");
+
+  printf("hello,"
+         " world\n");
+
+  char str[] = "hello, "
+               "world\n";
+
+  printf("%s", str);
+}
